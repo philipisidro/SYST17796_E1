99392931190f2cf1200763a58eefe8ae9f9e3f9d
phil <phil@localhost>
2022-02-06 22:31:42 -0500

    uncommented more code

---
+++ src/CardGame/Card.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CardGame;
+
+/**
+ *
+ * @author phil
+ */
+public class Card {
+    
+    
+    String word; 
+        
+    
+    public void setSuit(){
+        word = "word";
+    }
+    
+    public String getSuit(){
+        return word;
+    }
+    
+    public void setValue(){
+        
+    }
+    
+    public int getValue(){
+        return 5;
+    }
+}
--- src/CardGame/CardGame.java
+++ src/CardGame/CardGame.java
@@ -2,7 +2,6 @@ package CardGame;
 
 import java.util.Random;
 import java.util.Scanner;
-
 /**
  * This class models a simple card guessing game
  * 
@@ -15,9 +14,11 @@ public class CardGame {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-           
+        
+        //creates an array of Card of 7 
         Card[] hand = new Card[7];
            
+        //creates random
         Random random = new Random();
         
         for (int i = 0; i < hand.length; i++) {
@@ -33,24 +34,26 @@ public class CardGame {
 
         // Now ask the user for a card
         System.out.println("Pick a suit for your card");
-//        for (int i = 0; i < Card.SUITS.length; i++) {
-//            System.out.println((i + 1) + ": " + Card.SUITS[i]);
-//        }
+        
+        for (int i = 0; i < Card.SUITS.length; i++){
+            System.out.print((i+1) + ": " + Card.SUITS[i]);
+        }
+
         int suit = input.nextInt();
 
         System.out.println("Enter a value (1 to 13)");
         int value = input.nextInt();
-
-//        Card userGuess = new Card(value, Card.SUITS[suit - 1]);
+        
+        Card userGuess = new Card(value, Card.SUITS[suit - 1]);
 
         boolean match = false;
-//        for (Card card : hand) {
-//            if (card.getValue() == userGuess.getValue()
-//                    && card.getSuit().equals(userGuess.getSuit())) {
-//                match = true;
-//                break;
-//            }
-//        }
+        for (Card card : hand) {
+            if (card.getValue() == userGuess.getValue()
+                    && card.getSuit().equals(userGuess.getSuit())) {
+                match = true;
+                break;
+            }
+        }
     
         String response = match ? "Right guess": "No match";
         
