86f3d1b44ba47219583d466e282fadce49acd4ab
phil <phil@localhost>
2022-02-06 21:38:16 -0500

    uncommented more code

--- src/CardGame/CardGame.java
+++ src/CardGame/CardGame.java
@@ -1,4 +1,4 @@
-package Cardame;
+package CardGame;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -14,27 +14,22 @@ public class CardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           Scanner input = new Scanner(System.in);
-
-        // Create an array to hold 7 cards
-//        Card[] hand = new Card[7];
-
-        // We'll use Random to generate random numbers
+        Scanner input = new Scanner(System.in);
+           
+        Card[] hand = new Card[7];
+           
         Random random = new Random();
-
-//        for (int i = 0; i < hand.length; i++) {
-//            int value = random.nextInt(13) + 1;
-//            String suit = Card.SUITS[random.nextInt(4)];
-//
-//            Card card = new Card(value, suit);
-//            hand[i] = card;
-//        }
+        
+        for (int i = 0; i < hand.length; i++) {
+            int value = random.nextInt(13) + 1;
+            String suit = Card.SUITS[random.nextInt(4)];
+        }
 
         // print them out for debugging purposes
         System.out.println("Here are the cards in the hand");
-//        for (Card card : hand) {
-//            System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
-//        }
+        for (Card card : hand) {
+            System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
+        }
 
         // Now ask the user for a card
         System.out.println("Pick a suit for your card");
